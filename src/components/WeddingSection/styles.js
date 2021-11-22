a1e16f9ab02dce497e6cbcb63999a9e1a6e1f2a7
zfachrian <zuhadachmadfachrian@gmail.com>
2021-11-22 18:46:21 +0700

    change background

--- src/components/WeddingSection/styles.js
+++ src/components/WeddingSection/styles.js
@@ -1,5 +1,5 @@
 import { css } from '@emotion/core';
-import Background from '@assets/images/bg-wedding.jpg';
+import Background from '@assets/images/defaulltBack.jpg';
 
 export const styWrapper = css`
   background-image: url(${Background});
